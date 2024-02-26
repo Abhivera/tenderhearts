c46e7cba02c9f35be81fb83f9715c48c293fc754
Abhivera <abhijitverma2016@gmail.com>
2024-02-26 23:24:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Carousel from "./Carousel";
+import Header from "./Header";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <div>App</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Carousel/index.jsx
@@ -0,0 +1,62 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./styles.css";
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+const sliderImageUrl = [
+  
+  //Second image url
+  {
+    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+  },
+  //Third image url
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU",
+  },
+
+  //Fourth image url
+
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdvuww0JDC7nFRxiFL6yFiAxRJgM-1tvJTxA&usqp=CAU",
+  },
+];
+const Slider = () => {
+  return (
+    <div className="parent">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style"
+      >
+        {sliderImageUrl.map((imageUrl, index) => {
+          return (
+            <div className="slider" key={index}>
+              <img src={imageUrl.url} alt="movie" />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+export default Slider;
